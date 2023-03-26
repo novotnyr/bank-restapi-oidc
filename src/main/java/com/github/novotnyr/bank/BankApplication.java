30ab85fd027bf58c24d7d1d674a425d0e047a71b
Robert Novotny <novotnr0@gmail.com>
2023-03-27 01:13:40 +0200

    Add a REST API endpoint for bank account balance

--- src/main/java/com/github/novotnyr/bank/BankApplication.java
+++ src/main/java/com/github/novotnyr/bank/BankApplication.java
@@ -2,9 +2,20 @@ package com.github.novotnyr.bank;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigDecimal;
 
 @SpringBootApplication
+@RestController
 public class BankApplication {
+
+    @GetMapping("/accounts/{accountId}/balance")
+    public BigDecimal getBalance(@PathVariable String accountId) {
+        return BigDecimal.TEN;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(BankApplication.class, args);
