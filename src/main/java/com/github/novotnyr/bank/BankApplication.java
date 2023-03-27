f6320a0a6b9ccc50106d0b7183ee9931137bfcf4
Robert Novotny <novotnr0@gmail.com>
2023-03-27 11:30:29 +0200

    Use JWT token as an principal with authenticated data

--- src/main/java/com/github/novotnyr/bank/BankApplication.java
+++ src/main/java/com/github/novotnyr/bank/BankApplication.java
@@ -1,7 +1,11 @@
 package com.github.novotnyr.bank;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,9 +15,12 @@ import java.math.BigDecimal;
 @SpringBootApplication
 @RestController
 public class BankApplication {
+    public static final Logger logger = LoggerFactory.getLogger(BankApplication.class);
 
     @GetMapping("/accounts/{accountId}/balance")
-    public BigDecimal getBalance(@PathVariable String accountId) {
+    public BigDecimal getBalance(@PathVariable String accountId, @AuthenticationPrincipal Jwt jwt) {
+        String userId = (String) jwt.getClaims().getOrDefault("sub", "");
+        logger.info("Retrieving bank account balance: account: {}, user {}", accountId, userId);
         return BigDecimal.TEN;
     }
 
